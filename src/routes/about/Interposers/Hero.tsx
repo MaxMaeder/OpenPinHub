9da2fdd56112abd0b4f6d83a1948800acf5f5c12
Max Maeder <max.s.maeder@gmail.com>
2025-04-28 17:51:09 -0500

    Change badge size

--- src/routes/about/Interposers/Hero.tsx
+++ src/routes/about/Interposers/Hero.tsx
@@ -1,52 +1,71 @@
-import { Paper, Box, Title, Text, List, Button, Badge } from "@mantine/core";
+import {
+  Paper,
+  Box,
+  Title,
+  Text,
+  List,
+  Button,
+  Badge,
+  useMantineTheme,
+} from "@mantine/core";
 
 import classes from "./Hero.module.css";
+import { useMediaQuery } from "@mantine/hooks";
 
-const Hero = () => (
-  <Paper
-    withBorder
-    bd="3px solid white"
-    p="xl"
-    radius="xl"
-    className={classes.paper}
-  >
-    <Box className={classes.flexContainer}>
-      <Box className={classes.textContainer}>
-        <Title mb="lg">The OpenPin Interposer</Title>
-        <Text mb="lg">
-          You'll need an interposer to install OpenPin on your AI Pin. This is
-          the highest-quality one you can buy, made by the creator of OpenPin.
-        </Text>
-        <Text fw="bold">Features</Text>
-        <List mb="xl">
-          <List.Item>High-tolerance, extremely durable design</List.Item>
-          <List.Item>USB C connector (not pictured)</List.Item>
-          <List.Item>Premium braided cable included</List.Item>
-        </List>
-        <Button
-          size="lg"
-          component="a"
-          href="https://buy.stripe.com/cN2eYQ66U5PO3DObII"
-          target="_blank"
-          className={classes.button}
-        >
-          Buy Now
-        </Button>
+const Hero = () => {
+  const theme = useMantineTheme();
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.md})`);
+
+  return (
+    <Paper
+      withBorder
+      bd="3px solid white"
+      p="xl"
+      radius="xl"
+      className={classes.paper}
+    >
+      <Box className={classes.flexContainer}>
+        <Box className={classes.textContainer}>
+          <Title mb="lg">The OpenPin Interposer</Title>
+          <Text mb="lg">
+            You'll need an interposer to install OpenPin on your AI Pin. This is
+            the highest-quality one you can buy, made by the creator of OpenPin.
+          </Text>
+          <Text fw="bold">Features</Text>
+          <List mb="xl">
+            <List.Item>High-tolerance, extremely durable design</List.Item>
+            <List.Item>USB C connector (not pictured)</List.Item>
+            <List.Item>Premium braided cable included</List.Item>
+          </List>
+          <Button
+            size="lg"
+            component="a"
+            href="https://buy.stripe.com/cN2eYQ66U5PO3DObII"
+            target="_blank"
+            className={classes.button}
+          >
+            Buy Now
+          </Button>
+        </Box>
+        <Box className={classes.imageContainer}>
+          <Box
+            component="img"
+            src="/interposer.png"
+            className={classes.image}
+          />
+          <Badge
+            color="white"
+            c="black"
+            radius="sm"
+            size={isMobile ? "lg" : "xl"}
+            className={classes.badge}
+          >
+            V2.0
+          </Badge>
+        </Box>
       </Box>
-      <Box className={classes.imageContainer}>
-        <Box component="img" src="/interposer.png" className={classes.image} />
-        <Badge
-          color="white"
-          c="black"
-          radius="sm"
-          size="lg"
-          className={classes.badge}
-        >
-          V2.0
-        </Badge>
-      </Box>
-    </Box>
-  </Paper>
-);
+    </Paper>
+  );
+};
 
 export default Hero;
